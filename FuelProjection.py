9e75ec0d9602f1b47f1fe733d09e3d351f103984
Joseph Wallwork <j.wallwork16@imperial.ac.uk>
2017-09-10 14:09:14 +0100

    Small change in notation

--- FuelProjection.py
+++ FuelProjection.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib
 
-matplotlib.use('TkAgg')
+#matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
 
@@ -30,14 +30,14 @@ def biomass_quadrature_step(Pop0, CarCap, r, gr, time, h):
 
 # List countries, initial populations and growth rates:
 country = {1: 'Cuba', 2: 'Japan', 3: 'South Africa', 4: 'Uganda'}
-P0 = {1: 11.4e6, 2: 127e6, 3: 53.3e6, 4: 37.5e6, 5: 7.5e9}          # 5th is world population
+P0 = {0: 7.5e9, 1: 11.4e6, 2: 127e6, 3: 53.3e6, 4: 37.5e6}          # 0th entry corresponds to world population
 P_rate = {1: 1.39e-3, 2: 9.35e-3, 3: 1.57e-2, 4: 3.31e-2}
 
 # List initial fuel values and consumption rates:
-B0 = {1: 6.08e8, 2: 4.9e9, 3: 1.81e9, 4: 4.61e8, 5: 2.4e13}
-C0 = {1: 2.44e7, 2: 5.23e8, 3: 30e9, 4: 3.49e5, 5: 4.7e12}
-G0 = {1: 1.71e11, 2: 3.51e11, 3: 3.17e7, 4: 2.41e10, 5: 8.7e12}
-O0 = {1: 2.43e7, 2: 3.69e8, 3: 4.53e10, 4: 4.13e8, 5: 1.9e12}
+B0 = {1: 6.08e8, 2: 4.9e9, 3: 1.81e9, 4: 4.61e8, 0: 2.4e13}
+C0 = {1: 2.44e7, 2: 5.23e8, 3: 30e9, 4: 3.49e5, 0: 4.7e12}
+G0 = {1: 1.71e11, 2: 3.51e11, 3: 3.17e7, 4: 2.41e10, 0: 8.7e12}
+O0 = {1: 2.43e7, 2: 3.69e8, 3: 4.53e10, 4: 4.13e8, 0: 1.9e12}
 B_rate = {1: 1.39e-3, 2: 1.84e-3, 3: 1.89e-3, 4: 2.70}
 C_rate = {1: 1.91e-3, 2: 1.50, 3: 2.95, 4: 0.}
 G_rate = {1: 0.122, 2: 0.014, 3: 0.123, 4: 0.}
@@ -68,7 +68,7 @@ world_fossil_fuels = {1: WC, 2: WG, 3: WO}
 initial_fuels = {1: C0, 2: G0, 3: O0}
 fuel_rates = {1: C_rate, 2: G_rate, 3: O_rate}
 
-K = {1: 1.52e7, 2: 1.69e8, 3: 7.11e7, 4: 5e7, 5: 10e9}  # Carrying capacity of countries and world
+K = {1: 1.52e7, 2: 1.69e8, 3: 7.11e7, 4: 5e7, 0: 10e9}  # Carrying capacity of countries and world
 lam = 0.8  # Climate sensitivity
 
 # Choose population model and simulation duration:
@@ -81,7 +81,6 @@ T = int(raw_input('Simulation duration in years? (Default 100): ') or 100)
 # Other parameters:
 g = float(raw_input('Biomass growth rate? (Default 0.01): ') or 0.01)
 trees = bool(raw_input('Account for tree respiration? (Default False): ') or False)
-W = 7.5e9  # World population
 
 # Plotting setup:
 styles = {1: ':', 2: '--', 3: '-.', 4: '-'}
@@ -112,12 +111,12 @@ plt.clf()
 for i in country:
     if model == 'Malthus':
         for t in t_axis:
-            WP[i].append(P0[5] * np.exp(P_rate[i] * t))
+            WP[i].append(P0[0] * np.exp(P_rate[i] * t))
         print 't: ', np.shape(t_axis), 'WP: ', np.shape(WP[i])
         plt.semilogy(t_axis, WP[i], label=country[i], linestyle=styles[i])
     else:
         for t in t_axis:
-            WP[i].append((P0[5] * K[5] * np.exp(P_rate[i] * t)) / (K[5] + P0[5] * (np.exp(P_rate[i] * t) - 1)))
+            WP[i].append((P0[0] * K[0] * np.exp(P_rate[i] * t)) / (K[0] + P0[0] * (np.exp(P_rate[i] * t) - 1)))
         plt.semilogy(t_axis, WP[i], label=country[i], linestyle=styles[i])
 plt.gcf()
 if model == 'Malthus':
@@ -148,8 +147,8 @@ for fuel in fossil_fuels:
     plt.clf()
     for i in country:
         for t in t_axis:
-            world_fossil_fuels[fuel][i].append(initial_fuels[fuel][5] - fuel_rates[fuel][i]
-                                               * (WP[i][len(world_fossil_fuels[fuel][i])] - P0[5]) / P_rate[i])
+            world_fossil_fuels[fuel][i].append(initial_fuels[fuel][0] - fuel_rates[fuel][i]
+                                               * (WP[i][len(world_fossil_fuels[fuel][i])] - P0[0]) / P_rate[i])
         plt.semilogy(t_axis, fossil_fuels[fuel][i], label=country[i], linestyle=styles[i])
     plt.gcf()
     plt.legend(loc=4)
@@ -182,14 +181,14 @@ plt.clf()
 for i in country:
     if model == 'Malthus':
         for t in t_axis:
-            WB[i].append(B0[5] * np.exp(g * t)
-                         + B_rate[i] * (WP[i][len(WB[i])] - P0[5] * np.exp(g * t)) / (g - P_rate[i]))
+            WB[i].append(B0[0] * np.exp(g * t)
+                         + B_rate[i] * (WP[i][len(WB[i])] - P0[0] * np.exp(g * t)) / (g - P_rate[i]))
         plt.semilogy(t_axis, WB[i], label=country[i], linestyle=styles[i])
     else:
         I = 0
         for t in t_axis:
-            I += biomass_quadrature_step(P0[5], K[5], P_rate[i], g, t, 0.1)
-            WB[i].append(np.exp(g * t) * (B0[5] - B_rate[i] * P0[5] * K[5] * I))
+            I += biomass_quadrature_step(P0[0], K[0], P_rate[i], g, t, 0.1)
+            WB[i].append(np.exp(g * t) * (B0[0] - B_rate[i] * P0[0] * K[0] * I))
         plt.semilogy(t_axis, B[i], label=country[i], linestyle=styles[i])
 plt.gcf()
 plt.legend(loc=4)
@@ -201,11 +200,11 @@ plt.savefig('plots/world_bio_' + model + '_g={y1}.pdf'.format(y1=g), bbox_inches
 plt.clf()
 for i in country:
     WE0 = (B_CO2[trees] * B_rate[i] + C_CO2 * C_rate[i] + G_CO2 * G_rate[i] + O_CO2 * O_rate[i]) \
-          * (WP[i][0] - P0[5]) / P_rate[i]
+          * (WP[i][0] - P0[0]) / P_rate[i]
     WE[i].append(0)
     for t in t_axis[1:]:
         WE[i].append((B_CO2[trees] * B_rate[i] + C_CO2 * C_rate[i] + G_CO2 * G_rate[i] + O_CO2 * O_rate[i])
-                     * (WP[i][len(WE[i])] - P0[5]) / P_rate[i])
+                     * (WP[i][len(WE[i])] - P0[0]) / P_rate[i])
     plt.semilogy(t_axis, WE[i], label=country[i], linestyle=styles[i])
 plt.gcf()
 plt.legend(loc=4)
